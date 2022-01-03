bf5888988a1067b9dafbee25763021f8b8833452
IshanShetty <ishan.h.shetty@gmail.com>
2022-01-03 17:57:39 -0600

    remove debugging and update readme

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("paste-wrapper").focus();
 
     let inFrame = window.location !== window.parent.location;
-    console.log("Is in iFrame?: " + inFrame);
     editor = new CodeMirror(document.getElementById('paste-wrapper'), {
         lineNumbers: true,
         theme: 'one-dark',
